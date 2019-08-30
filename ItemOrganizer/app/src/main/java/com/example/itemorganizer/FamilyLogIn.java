7c2d135f93b0e1b4f64edf865769a7a8ac829620
Asil Mian <asilmian@gmail.com>
2019-08-30 14:02:18 +1000

    New family feature added

--- ItemOrganizer/app/src/main/java/com/example/itemorganizer/FamilyLogIn.java
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/FamilyLogIn.java
@@ -1,7 +1,11 @@
 package com.example.itemorganizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FamilyLogIn extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class FamilyLogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family_log_in);
+    }
+
+    //go to new family page
+    public void goNewFamily(View view) {
+        Intent intent = new Intent(this, NewFamily.class);
+        startActivity(intent);
+    }
+
+    //send family Token to verify
+    public void sendToken(View view){
+        EditText editText = findViewById(R.id.familyToken);
+        String message = editText.getText().toString();
+        //clear text box
+        editText.setText(" ", TextView.BufferType.EDITABLE);
+        //send token
     }
 }
---
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/NewFamily.java
@@ -0,0 +1,25 @@
+package com.example.itemorganizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class NewFamily extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_family);
+    }
+
+    //create family
+    public void createFamily(View view){
+        EditText editText = findViewById(R.id.familyName);
+        String message = editText.getText().toString();
+        //clears text box
+        editText.setText(" ", TextView.BufferType.EDITABLE);
+        //send details to create new family
+    }
+}
