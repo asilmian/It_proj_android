a6ca756621d98283d7009ab44883632e50d537bf
buddykaroon <buddykaroon@gmail.com>
2019-09-02 13:37:39 +1000

    first commit

--- ItemOrganizer/app/src/main/java/com/example/itemorganizer/MainActivity.java
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.itemorganizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button EnterBtn = (Button) findViewById(R.id.enter_btn);
+        EnterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openLogin();
+            }
+        });
+    }
+    public void openLogin(){
+
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
     }
 }
