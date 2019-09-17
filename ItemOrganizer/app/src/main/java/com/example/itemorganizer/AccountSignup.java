c1dfe2da29fda1dc9d669d17c01a6111d4e9d559
buddykaroon <buddykaroon@gmail.com>
2019-09-17 21:41:40 +1000

    updated new_family to contain account prototype

--- ItemOrganizer/app/src/main/java/com/example/itemorganizer/AccountSignup.java
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/AccountSignup.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class AccountSignup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_account_signup);
     }
-
-    public void goAccountLogin(View view) {
-        Intent intent = new Intent(this, AccountLogin.class);
+    public void openFamily(View view){
+        Intent intent = new Intent(this, FamilyLogIn.class);
         startActivity(intent);
     }
+
 }
