df79a063ec233ebb2ec7d457b65f8352d5ccde28
Asil Mian <asilmian@gmail.com>
2019-09-21 19:57:14 +1000

    auth in progress

--- ItemOrganizer/app/src/main/java/com/example/itemorganizer/AccountLogin.java
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/AccountLogin.java
@@ -7,18 +7,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AccountLogin extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_login);
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
     public void openSignup(View view){
 
         Intent intent = new Intent(this, AccountSignup.class);
         startActivity(intent);
     }
+
 
     public void openHomepage(View view){
 
