f4946b63cc5ec7c5b45ed9e54f888af93dadda7e
Asil Mian <asilmian@gmail.com>
2019-09-21 19:39:52 +1000

    upgrade to androidx

--- ItemOrganizer/app/src/main/java/com/example/itemorganizer/FamilyLogIn.java
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/FamilyLogIn.java
@@ -1,11 +1,13 @@
 package com.example.itemorganizer;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FamilyLogIn extends AppCompatActivity {
 
