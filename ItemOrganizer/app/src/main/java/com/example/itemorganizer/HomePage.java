f000255411fd6f4b736474b3b422d5c0c558aa8f
buddykaroon <buddykaroon@gmail.com>
2019-09-23 22:50:40 +1000

    Working on making the Buttons go to different places.

--- ItemOrganizer/app/src/main/java/com/example/itemorganizer/HomePage.java
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/HomePage.java
@@ -1,6 +1,7 @@
 package com.example.itemorganizer;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,8 +9,11 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class HomePage extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +22,31 @@ public class HomePage extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.draw_layout);
+
+//      Set navigation button listeners.
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) //Check which items are clicked.
+        {
+            //Check for account.
+            case R.id.nav_account:
+       //         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //    Code for the menu toggle button on the top left
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START))
