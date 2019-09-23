f5e882f96fe81072b3bd757783ab43efbaf45c3e
buddykaroon <buddykaroon@gmail.com>
2019-09-23 22:13:07 +1000

    Homepage navbar mockup complete. Added Fake Items in, to prototype scrolling

--- ItemOrganizer/app/src/main/java/com/example/itemorganizer/HomePage.java
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/HomePage.java
@@ -1,14 +1,37 @@
 package com.example.itemorganizer;
 
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-public class HomePage extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.draw_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
 }
