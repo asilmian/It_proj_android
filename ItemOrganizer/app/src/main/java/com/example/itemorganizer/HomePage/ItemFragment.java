9bcaf2fed5f388cffb1bb28290f283284114f961
buddykaroon <buddykaroon@gmail.com>
2019-10-04 14:55:03 +1000

    Added ItemFragment functionality (untested)

--- ItemOrganizer/app/src/main/java/com/example/itemorganizer/HomePage/ItemFragment.java
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/HomePage/ItemFragment.java
@@ -2,10 +2,13 @@ package com.example.itemorganizer.HomePage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.itemorganizer.BackendItem;
+import com.example.itemorganizer.BackendReq;
 import com.example.itemorganizer.CameraActivity;
 import com.example.itemorganizer.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,8 +16,24 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ItemFragment extends Fragment {
+
+
+    private final static String TAG = FamilyFragment.class.toString();
+
+    private RecyclerView recyclerView;
+    private ItemRAdapter mAdapter;
+
+    private final static String URL = "http://167.71.243.144:5000/user/info/families";
 
     @Nullable
     @Override
@@ -30,6 +49,60 @@ public class ItemFragment extends Fragment {
             }
         });
 
+        initRecyclerView(view);
+        showItems();
         return view;
+    }
+
+    private void initRecyclerView(View view){
+        recyclerView = view.findViewById(R.id.item_recycler);
+
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        this.mAdapter = new ItemRAdapter(new ArrayList<ArrayList<String>>());
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    private void showItems(){
+        //get fams
+        BackendItem get_req = new BackendItem(URL, BackendReq.GET);
+        get_req.setHeaders(new HashMap<String, String>());
+        Log.d(TAG,get_req.getHeaders().toString());
+        BackendReq.send_req(get_req);
+        //Items = []
+        ArrayList<ArrayList<String>> items = getItemData(get_req);
+
+
+        //put items on display
+        for (ArrayList<String> item : items){
+            this.mAdapter.addAndNotify(item);
+        }
+    }
+
+
+    private ArrayList<ArrayList<String>> getItemData(BackendItem req){
+        //Array of Data
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+
+        try{
+            JSONObject raw_data = new JSONObject(req.getResponse());
+            JSONArray keys  = raw_data.names();
+
+            for (int i=0; i<keys.length(); i++){
+                String key = keys.getString(i);
+                ArrayList<String> tempData = new ArrayList<>();
+
+                tempData.add(raw_data.getJSONObject(key).getString("name")); // First element
+                tempData.add(raw_data.getJSONObject(key).getString("desc")); // Second Element
+                tempData.add(raw_data.getJSONObject(key).getString("tags")); // Third Element
+                tempData.add(raw_data.getJSONObject(key).getString("tags")); // Third Element
+
+                result.add(tempData);
+            }
+        } catch (Exception e){
+            Log.e(TAG, e.toString());
+        }
+        return result;
     }
 }
---
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/HomePage/ItemRAdapter.java
@@ -0,0 +1,89 @@
+package com.example.itemorganizer.HomePage;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.itemorganizer.R;
+
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemRAdapter extends RecyclerView.Adapter<ItemRAdapter.ItemViewHolder> {
+
+    private static final String TAG = "ItemRAdapater";
+
+    private ArrayList<ArrayList<String>> items;
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder{
+        public TextView name;
+        public TextView desc;
+        public TextView tags;
+        public CardView cardView;
+        public ItemViewHolder(View v){
+            super(v);
+            name = v.findViewById(R.id.item_card_name);
+            desc = v.findViewById(R.id.item_card_description);
+            tags = v.findViewById(R.id.item_card_tags);
+            cardView = v.findViewById(R.id.item_card_image);
+        }
+    }
+
+    public ItemRAdapter(ArrayList<ArrayList<String>>  items){
+        this.items = items;
+    }
+
+    @Override
+    public ItemRAdapter.ItemViewHolder onCreateViewHolder(ViewGroup parent,
+                                                           int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_card_view, parent, false);
+
+        ItemViewHolder vh = new ItemViewHolder(v);
+        return vh;
+    }
+
+
+    // called each time a new item is added to the RecyclerView container
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder viewHolder, int i) {
+
+        Log.d(TAG, "onBindViewHolder: called with i: " + i);
+
+        viewHolder.name.setText(items.get(i).get(0)); //Name
+        viewHolder.desc.setText(items.get(i).get(1)); //Description
+        viewHolder.tags.setText(items.get(i).get(2)); //Tags
+
+
+        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Waddup", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: size: " + items.size());
+        return items.size();
+    }
+
+
+    /*
+     *   Public exposed functionality
+     */
+    public void addAndNotify(ArrayList<String> added){
+        items.add(items.size(), added);
+        notifyItemInserted(items.size());
+    }
+}
