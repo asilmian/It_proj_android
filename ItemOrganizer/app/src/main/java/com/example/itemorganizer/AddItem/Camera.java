a9c766dbab03b2b0feb848d2b3a3092d9253857b
buddykaroon <buddykaroon@gmail.com>
2019-10-22 11:25:44 +1100

    Testing adding camera to fragment profile

---
+++ ItemOrganizer/app/src/main/java/com/example/itemorganizer/AddItem/Camera.java
@@ -0,0 +1,16 @@
+package com.example.itemorganizer.AddItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itemorganizer.R;
+
+public class Camera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
